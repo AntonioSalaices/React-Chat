cd92a7d4cfc7f239b5c5134ef531b894749cecb6
AntonioSalaices <antonio.salaicesm@gmail.com>
2023-06-09 15:23:44 -0700

    [FIX] fixed issue related to react import using jest

--- src/__test__/App.spec.tsx
+++ src/__test__/App.spec.tsx
@@ -1,5 +1,6 @@
+import * as React from "react" 
 import { render } from '@testing-library/react';
-import App from '../App';
+import App from '../App.tsx';
 
 describe('<App />', () => {
     test('App component should render correctly', () => {
